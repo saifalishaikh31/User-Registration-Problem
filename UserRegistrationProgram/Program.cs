974b98ecf734bb3638f64e84b25769c28023e6fa
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-17 14:33:00 +0530

    [Saif Ali] Add : Validate First Name Added

---
+++ UserRegistrationProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Program");
+            Validations validate = new Validations();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("1.Email Validation."
+                              + "\n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        validate.ValidateFirstName();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter Correct Option!!!");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ UserRegistrationProgram/Validations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    internal class Validations
+    {
+        public void ValidateFirstName()
+        {
+            Regex reg = new Regex("^[A-Z]{1}[a-z]{2,}$");
+            Console.Write("Enter First Name : ");
+            string fname = Console.ReadLine();
+            if (reg.IsMatch(fname) && fname.Length >= 3)
+            {
+                Console.WriteLine("Your First Name is : {0}", fname);
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid First Name!!!");
+                Console.WriteLine("It should start with 'Capital letter' and must be greater than 2 words!!!");
+            }
+        }
+    }
+}

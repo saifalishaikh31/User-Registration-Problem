6fe8be5560b05d288f5e43b79611cb9b7614fa30
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-03-03 17:12:00 +0530

    [Saif Ali] Add : Refactor the code to throw custom exception in case of Invalid User Details Added

---
+++ UserRegistrationProgram/CustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    public class CustomException : Exception
+    {
+        public enum CustomExceptionType
+        {
+            INVALID_FIRSTNAME,
+            INVALID_LASTNAME,
+            INVALID_EMAIL_ADDRESS,
+            INVALID_PHONE_NUMBER,
+            INVALID_PASSWORD
+        }
+        public CustomExceptionType customExceptionType;
+        public CustomException(CustomExceptionType type, string message) : base(message)
+        {
+            this.customExceptionType = type;
+        }
+    }
+}
--- UserRegistrationProgram/Validations.cs
+++ UserRegistrationProgram/Validations.cs
@@ -12,84 +12,124 @@ namespace UserRegistrationProgram
        
         public string ValidateFirstName(string firstName)
         {
-            Regex reg = new Regex("^[A-Z]{1}[a-z]{2,}$");
-            if (reg.IsMatch(firstName) && firstName.Length >= 3)
+            try
             {
-                Console.WriteLine("Your First Name is : {0}", firstName);
-                return firstName;
+                Regex reg = new Regex("^[A-Z]{1}[a-z]{2,}$");
+                if (reg.IsMatch(firstName) && firstName.Length >= 3)
+                {
+                    Console.WriteLine("Your First Name is : {0}", firstName);
+                    return firstName;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid First Name!!!");
+                    Console.WriteLine("Error : It should start with 'Capital letter' and must be greater than 2 words!!!\n");
+                    //return "Enter Valid First Name";
+                    throw new CustomException(CustomException.CustomExceptionType.INVALID_FIRSTNAME, "Enter Valid First Name");
+                }
             }
-            else
+            catch(CustomException ex)
             {
-                Console.WriteLine("Enter Valid First Name!!!");
-                Console.WriteLine("Error : It should start with 'Capital letter' and must be greater than 2 words!!!\n");
-                return "Enter Valid First Name";
+                return ex.Message;
             }
         }
 
         public string ValidateLastName(string lastName)
         {
-            Regex reg = new Regex("^[A-Z]{1}[a-z]{2,}$");
-            if (reg.IsMatch(lastName) && lastName.Length >= 3)
+            try
             {
-                Console.WriteLine("Your Last Name is : {0}", lastName);
-                return lastName;
+                Regex reg = new Regex("^[A-Z]{1}[a-z]{2,}$");
+                if (reg.IsMatch(lastName) && lastName.Length >= 3)
+                {
+                    Console.WriteLine("Your Last Name is : {0}", lastName);
+                    return lastName;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Last Name!!!");
+                    Console.WriteLine("Error : It should start with 'Capital letter' and must be greater than 2 words!!!\n");
+                    //return "Enter Valid Last Name";
+                    throw new CustomException(CustomException.CustomExceptionType.INVALID_LASTNAME, "Enter Valid Last Name");
+                }
             }
-            else
+            catch(CustomException ex)
             {
-                Console.WriteLine("Enter Valid Last Name!!!");
-                Console.WriteLine("Error : It should start with 'Capital letter' and must be greater than 2 words!!!\n");
-                return "Enter Valid Last Name";
+                return ex.Message;
             }
         }
 
         public string ValidateEmail(string email)
         {
-            Regex reg = new Regex("^[A-Z0-9a-z]{1,}([~!&%.#$^_+-][A-Za-z0-9]+)*[@][A-Za-z0-9]{1,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2,3})?$");                       
-            if (reg.IsMatch(email))
+            try
             {
-                Console.WriteLine("Your Email Id is : {0}", email);
-                return email;
+                Regex reg = new Regex("^[A-Z0-9a-z]{1,}([~!&%.#$^_+-][A-Za-z0-9]+)*[@][A-Za-z0-9]{1,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2,3})?$");
+                if (reg.IsMatch(email))
+                {
+                    Console.WriteLine("Your Email Id is : {0}", email);
+                    return email;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Email Id!!!");
+                    Console.WriteLine("Error : It should contain atleast one 'Capital letter' and one 'Small letter' and one 'Number'!!!\n");
+                    //return "Enter Valid Email";
+                    throw new CustomException(CustomException.CustomExceptionType.INVALID_EMAIL_ADDRESS, "Enter Valid Email");
+                }
             }
-            else
+            catch(CustomException ex)
             {
-                Console.WriteLine("Enter Valid Email Id!!!");
-                Console.WriteLine("Error : It should contain atleast one 'Capital letter' and one 'Small letter' and one 'Number'!!!\n");
-                return "Enter Valid Email";
+                return ex.Message;
             }
         }
 
         public string ValidateMobileNumber(string mobileNo)
         {
-            Regex reg = new Regex("^[9][1][ ]{1}([6-9]{1}[0-9]{9})$");
-            if (reg.IsMatch(mobileNo))
+            try
             {
-                Console.WriteLine("Your Mobile Number is : {0}", mobileNo);
-                return mobileNo;
+                Regex reg = new Regex("^[9][1][ ]{1}([6-9]{1}[0-9]{9})$");
+                if (reg.IsMatch(mobileNo))
+                {
+                    Console.WriteLine("Your Mobile Number is : {0}", mobileNo);
+                    return mobileNo;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Mobile Number!!!");
+                    Console.WriteLine("Error : It should start with '91' and must contain total 12 Digits For eg : 91 9287654356 \n");
+                    //return "Enter Valid Mobile Number";
+                    throw new CustomException(CustomException.CustomExceptionType.INVALID_PHONE_NUMBER, "Enter Valid Mobile Number");
+                }
             }
-            else
+            catch(CustomException ex)
             {
-                Console.WriteLine("Enter Valid Mobile Number!!!");
-                Console.WriteLine("Error : It should start with '91' and must contain total 12 Digits For eg : 91 9287654356 \n");
-                return "Enter Valid Mobile Number";
+                return ex.Message;
             }
         }
         public string ValidatePassword(string password)
         {
-           Regex reg = new Regex("^[A-Za-z0-9]{8,}([A-Z0-9][~!&%.#$^_+-]{1})$");
-           //Regex reg = new Regex("(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%]).[A-Z0-9a-z@#$%]{8,}");
-            if (reg.IsMatch(password))
+            try
             {
-                Console.WriteLine("Your Password is : {0}", password);
-                return password;
+                Regex reg = new Regex("^[A-Za-z0-9]{8,}([A-Z0-9][~!&%.#$^_+-]{1})$");
+                //Regex reg = new Regex("(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%]).[A-Z0-9a-z@#$%]{8,}");
+                if (reg.IsMatch(password))
+                {
+                    Console.WriteLine("Your Password is : {0}", password);
+                    return password;
+                }
+                else
+                {
+                    Console.WriteLine("Enter Valid Password!!!");
+                    Console.WriteLine("Error : It should atleat contain 8 digits"
+                                    + "\nand atleat one 'Upper Case Letter'"
+                                    + "\nand atleast one 'Numeric Number' "
+                                    + "\nand exactly one 'special character' (eg : ~ ! & % . # $ ^ _ + - )\n");
+                    //return "Enter Valid Password";
+                    throw new CustomException(CustomException.CustomExceptionType.INVALID_PASSWORD, "Enter Valid Password");
+                }
             }
-            else
+            catch(CustomException ex)
             {
-                Console.WriteLine("Enter Valid Password!!!");
-                Console.WriteLine("Error : It should atleat contain 8 digits"
-                                +"\nand atleat one 'Upper Case Letter'"
-                                +"\nand atleast one 'Numeric Number' "
-                                +"\nand exactly one 'special character' (eg : ~ ! & % . # $ ^ _ + - )\n");
-                return "Enter Valid Password";
+                return ex.Message;
             }
         }
 
